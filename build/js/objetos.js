4693b65a4f54b1ad02cdc80b36d531894d0f5aaf
GralSpringgan <mmaurixd1@hotmail.com>
2024-08-27 20:38:40 -0300

    test_icons

---
+++ build/js/objetos.js
@@ -0,0 +1,78 @@
+//------------------CARRUSEL-HEADER----------------------------//
+const arrayImg = [
+
+    {src: "/src/img/header/test_fondo.jpg" },
+    {src: "/src/img/header/fondo_header1.jpg"},
+    {src: "/src/img/header/fondo_header2.jpg"},
+    {src: "/src/img/header/fondo_header3.jpg"}
+
+];
+
+
+//-------------------------IMAGENES SERVICIOS Y CURSOS-----------------------------//
+
+const imgCursos = [
+    {
+        img: "src/img/icons/barbero.png",
+        nombre : 'Barbero',
+        descripcion: 'Corte clasico masculino',
+        parrafo:'Cortes de cabello tradicionales y elegantes para hombres. Este servicio incluye estilos atemporales como el corte a máquina, degradados suaves, y cortes peinados hacia un lado o con raya, todos realizados con atención al detalle y precisión.'
+    },
+    {
+        img: "src/img/icons/sombreado.png",
+        nombre : 'Sombreado',
+        descripcion: 'Sombreados',
+        parrafo:'Un experto en barbería ofrece enseñanza especializada en técnicas de sombreado, enfocada en dominar degradados y transiciones suaves en cortes de cabello.'
+    },
+    {
+        img: "src/img/icons/navaja.png",
+        nombre : 'Navaja',
+        descripcion: 'Uso adecuado de navaja',
+        parrafo:'técnicas avanzadas de corte con navaja, desde el delineado fino hasta el afeitado clásico, asegurando un acabado limpio y profesional.'
+    },
+    {
+        img: "src/img/icons/postura.png",
+        nombre : 'Postura',
+        descripcion: 'Postura Correcta',
+        parrafo:'Cómo mantener una posición adecuada del cuerpo durante el trabajo, para prevenir lesiones y fatiga. A través de técnicas prácticas y ajustes personalizados, capacito a los barberos para que mantengan una postura saludable mientras realizan cortes de cabello, afeitados y otros servicios.'
+    },
+    {
+        img: "src/img/icons/corte pelo.png",
+        nombre : 'Tijera',
+        descripcion: 'Todo tijera al detalle',
+        parrafo:'Formación especializada en el uso detallado de tijeras, enfoque en técnicas avanzadas de corte con tijera, incluyendo precisión en la técnica de corte, manejo experto de las tijeras para lograr texturización y acabado fino, y ajustes personalizados según el estilo deseado.'
+    },
+    {
+        img: "src/img/icons/barba etc.png",
+        nombre : 'etc',
+        descripcion: 'Barba, cejas, texturado',
+        parrafo:'Formación integral en el cuidado de barba, cejas y texturizado del cabello. Capacito a los barberos en el uso de herramientas específicas, técnicas de precisión y enfoques personalizados para resaltar las características del cliente, garantizando un estilo limpio, bien definido y a la moda.'
+    }
+];
+
+const imgServicios =[
+    {
+        img:'src/img/icons/corte-de-pelo.png',
+        nombre:'corte-pelo',
+        descripcion:'Corte todo tijera',
+        parrafo:'Transforma tu look con un corte a tijera preciso y estilizado. Con técnicas avanzadas y atención al detalle, te ofrezco un servicio personalizado que resalta tu estilo único, garantizando un acabado natural y sofisticado. ¡Reserva tu cita y experimenta la diferencia de un corte a tijera profesional!'
+    },
+    {
+        img:'src/img/icons/sombras.png',
+        nombre:'sombreados',
+        descripcion:'Sombreados',
+        parrafo:'Realza tu estilo con un sombreado impecable que define y estructura tu corte. Con destreza y precisión, logro transiciones suaves que destacan tu look, creando un acabado limpio y moderno. Confía en mi experiencia para darle a tu cabello el sombreado perfecto. ¡Reserva tu cita y luce un estilo impecable!'
+    },
+    {
+        img:'src/img/icons/barbas.png',
+        nombre:'barba',
+        descripcion: 'Barba',
+        parrafo:'Cuida tu barba con un servicio profesional que combina estilo y precisión. Desde recortes limpios hasta contornos definidos, me aseguro de que tu barba luzca impecable y bien cuidada. Ya sea que busques un look clásico o moderno, estoy aquí para ayudarte a alcanzar el estilo que deseas. ¡Reserva tu cita y dale a tu barba la atención que se merece!'
+    },
+    {
+        img:'src/img/icons/ceja.png',
+        nombre:'cejas',
+        descripcion:'Cejas',
+        parrafo:'Define tu mirada con un servicio profesional de arreglo de cejas. Con técnicas precisas y personalizadas, doy forma a tus cejas para resaltar tus facciones y lograr un look equilibrado y elegante. Ya sea un diseño sutil o una corrección detallada, estoy aquí para que tus cejas luzcan perfectas. ¡Reserva tu cita y potencia tu expresión!'
+    }
+];
